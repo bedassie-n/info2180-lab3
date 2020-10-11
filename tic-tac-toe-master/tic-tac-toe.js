b95a146a1c46d1775041fc2ae96dc567789d9fae
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2020-10-11 12:57:35 -0500

    exercise 2

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -1,6 +1,18 @@
 window.onload = function(){
-    var squares = document.getElementById("board").children;
-    Array.from(squares).forEach(element => {
+    var squares = Array.from(document.getElementById("board").children);
+    var state = [];
+    squares.forEach(element => {
         element.classList.add("square");
+        element.addEventListener("click", function(){
+            if (state.length % 2 == 0){
+                element.classList.add("X");
+                element.innerHTML = "X";
+                state.push("X");
+            } else {
+                element.classList.add("O");
+                element.innerHTML = "O";
+                state.push("O");
+            }
+        })
     });
 };
